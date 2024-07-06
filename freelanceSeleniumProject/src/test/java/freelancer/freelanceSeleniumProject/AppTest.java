f36b578723654d82dc24846d888f737dfc08db25
Nikhil Purwant <purwantn@gmail.com>
2024-07-06 22:47:04 +0530

    Framework structure
    Framework structure

---
+++ freelanceSeleniumProject/src/test/java/freelancer/freelanceSeleniumProject/AppTest.java
@@ -0,0 +1,26 @@
+package freelancer.freelanceSeleniumProject;
+
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue()
+	{
+		assertTrue( true );
+		System.out.println("Nikhil");
+	}
+}
